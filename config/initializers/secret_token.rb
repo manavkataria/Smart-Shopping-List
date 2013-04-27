e4ea2653cfda4b1257cfe154ec12b6a5493243be
Manav Kataria <manavkataria.iitb@gmail.com>
2013-04-27 04:40:19 -0500

    rails new .
    Untouched.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartShoppingList::Application.config.secret_token = '0ecf4743f4644cc6a50f4202231a14e4883fe112c409e5385a120b1daf38ba51c1f16616e9cd27d8e33d71d98c7c1ecbc2df773698ebc1290802c06d43451041'
